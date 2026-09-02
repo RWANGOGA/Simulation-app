934578e248f32d3a0ea850081f8b9f7d12d317a8
madrinejean123 <jeanmadrinenamulinde@gmail.com>
2026-09-02 12:11:14 +0300

    3d body

--- simtack/lib/l10n/app_localizations_xog.dart
+++ simtack/lib/l10n/app_localizations_xog.dart
@@ -4,9 +4,9 @@ import 'app_localizations.dart';
 
 // ignore_for_file: type=lint
 
-/// The translations for English (`en`).
-class AppLocalizationsEn extends AppLocalizations {
-  AppLocalizationsEn([String locale = 'en']) : super(locale);
+/// The translations for Soga (`xog`).
+class AppLocalizationsXog extends AppLocalizations {
+  AppLocalizationsXog([String locale = 'xog']) : super(locale);
 
   @override
   String get chooseLanguageTitle => 'Choose Your Language';
