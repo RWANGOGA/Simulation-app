68f155e42e1cafb048ac7107262b878e3c162d2c
RWANGOGA <akandwanahojonan256@gmail.com>
2026-09-16 12:55:59 +0300

    fixes for patient screen

--- simtack/lib/features/dashboard/ui/reports_screen.dart
+++ simtack/lib/features/dashboard/ui/reports_screen.dart
@@ -10,7 +10,6 @@ import '../../../core/network/api_client.dart';
 import '../../../core/theme/app_palette.dart';
 import '../../../core/theme/app_header_bar.dart';
 import '../../../l10n/app_localizations.dart';
-import 'practitioner_scaffold.dart';
 
 const List<Color> _chartPalette = [
   Color(0xFF6D28D9),
