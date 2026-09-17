dc4c4eebc0a7e1a3a05a3dff9a73cbc95cb2706f
madrinejean123 <jeanmadrinenamulinde@gmail.com>
2026-09-17 18:19:14 +0300

    patient

--- simtack/lib/l10n/app_localizations_xog.dart
+++ simtack/lib/l10n/app_localizations_xog.dart
@@ -494,9 +494,6 @@ class AppLocalizationsXog extends AppLocalizations {
   String get removeTooltip => 'Remove';
 
   @override
-  String get closeButtonTooltip => 'Close';
-
-  @override
   String get addAnotherLocationLabel => 'Add another location';
 
   @override
